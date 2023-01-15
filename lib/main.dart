d6977431139e074030b7a64857c6232a4b2d1d4a
ananalfredcarlos@gmail.com <ananalfredcarlos@gmail.com>
2023-01-15 13:44:40 +0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kopifood/firebase_options.dart';
 import 'package:kopifood/injection.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   configureDependencies();
   runApp(MyApp());
 }
